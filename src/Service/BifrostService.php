57a8c9ef095e1a3997e6d6b276de15a010c32523
Chris Penny <chris.penny@gmail.com>
2024-06-06 12:31:11 +1200

    Use EnterpriseSearchService. Provide one examples of overridng a Request

--- src/Service/BifrostService.php
+++ src/Service/BifrostService.php
@@ -2,73 +2,12 @@
 
 namespace SilverStripe\SearchServiceBifrost\Service;
 
-use Elastic\EnterpriseSearch\Client;
-use Exception;
-use Psr\Log\LoggerInterface;
-use SilverStripe\Core\Config\Configurable;
 use SilverStripe\Core\Environment;
-use SilverStripe\Core\Injector\Injectable;
 use SilverStripe\SearchService\Exception\IndexConfigurationException;
-use SilverStripe\SearchService\Interfaces\BatchDocumentRemovalInterface;
-use SilverStripe\SearchService\Interfaces\DocumentInterface;
-use SilverStripe\SearchService\Interfaces\IndexingInterface;
-use SilverStripe\SearchService\Service\DocumentBuilder;
-use SilverStripe\SearchService\Service\IndexConfiguration;
-use SilverStripe\SearchService\Service\Traits\ConfigurationAware;
+use SilverStripe\SearchServiceElastic\Service\EnterpriseSearchService;
 
-class BifrostService implements IndexingInterface, BatchDocumentRemovalInterface
+class BifrostService extends EnterpriseSearchService
 {
-
-    use Configurable;
-    use ConfigurationAware;
-    use Injectable;
-
-    private const DEFAULT_FIELD_TYPE = 'text';
-
-    private Client $client;
-
-    private DocumentBuilder $builder;
-
-    private ?LoggerInterface $logger = null;
-
-    private static int $max_document_size = 102400;
-
-    private static array $dependencies = [
-        'client' => '%$' . Client::class . '.indexingInterface',
-        'configuration' => '%$' . IndexConfiguration::class,
-        'builder' => '%$' . DocumentBuilder::class,
-        'logger' => '%$' . LoggerInterface::class . '.errorhandler',
-    ];
-
-    public function setClient(Client $client): static
-    {
-        $this->client = $client;
-
-        return $this;
-    }
-
-    public function setBuilder(DocumentBuilder $builder): static
-    {
-        $this->builder = $builder;
-
-        return $this;
-    }
-
-    public function setLogger(?LoggerInterface $logger): void
-    {
-        $this->logger = $logger;
-    }
-
-    public function environmentizeIndex(string $indexName): string
-    {
-        $variant = IndexConfiguration::singleton()->getIndexVariant();
-
-        if ($variant) {
-            return sprintf('%s-%s', $variant, $indexName);
-        }
-
-        return $indexName;
-    }
 
     public function getExternalURL(): ?string
     {
@@ -82,88 +21,7 @@ class BifrostService implements IndexingInterface, BatchDocumentRemovalInterface
 
     public function getDocumentationURL(): ?string
     {
-        return 'TBA';
-    }
-
-    public function addDocument(DocumentInterface $document): ?string
-    {
-        return 'document_id_1';
-    }
-
-    /**
-     * @param DocumentInterface[] $documents
-     */
-    public function addDocuments(array $documents): array
-    {
-        return [
-            'document_id_1',
-            'document_id_2',
-        ];
-    }
-
-    public function removeDocument(DocumentInterface $document): ?string
-    {
-        return 'document_id_1';
-    }
-
-    /**
-     * @param DocumentInterface[] $documents
-     */
-    public function removeDocuments(array $documents): array
-    {
-        return [
-            'document_id_1',
-            'document_id_2',
-        ];
-    }
-
-    /**
-     * Forcefully remove all documents from the provided index name. Batches the requests to Elastic based upon the
-     * configured batch size, beginning at page 1 and continuing until the index is empty.
-     *
-     * @param string $indexName The index name to remove all documents from
-     * @return int The total number of documents removed
-     */
-    public function removeAllDocuments(string $indexName): int
-    {
-        return 0;
-    }
-
-    public function getMaxDocumentSize(): int
-    {
-        return $this->config()->get('max_document_size');
-    }
-
-    public function getDocument(string $id): ?DocumentInterface
-    {
-        return null;
-    }
-
-    /**
-     * @return DocumentInterface[]
-     */
-    public function getDocuments(array $ids): array
-    {
-        return [];
-    }
-
-    /**
-     * @return DocumentInterface[]
-     * @throws Exception
-     */
-    public function listDocuments(string $indexName, ?int $pageSize = null, int $currentPage = 0): array
-    {
-        return [];
-    }
-
-    public function getDocumentTotal(string $indexName): int
-    {
-        return 0;
-    }
-
-    public function configure(): array
-    {
-        return [];
+        return sprintf('%s/docs', $this->getExternalURL());
     }
 
     /**
@@ -171,9 +29,9 @@ class BifrostService implements IndexingInterface, BatchDocumentRemovalInterface
      */
     public function validateField(string $field): void
     {
-        if ($field[0] === '_') {
+        if ($field[0] === '_' && $field !== '_attachment') {
             throw new IndexConfigurationException(sprintf(
-                'Invalid field name: %s. Fields cannot begin with underscores.',
+                'Invalid field name: %s. "_attachment" is the only field that can begin with an underscore.',
                 $field
             ));
         }
---
+++ src/Service/Requests/GetSchema.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace SilverStripe\SearchServiceBifrost\Service\Requests;
+
+use Elastic\EnterpriseSearch\AppSearch\Request\GetSchema as AppSearchGetSchema;
+
+class GetSchema extends AppSearchGetSchema
+{
+
+    public function __construct(string $engineName)
+    {
+        parent::__construct($engineName);
+
+        $this->path = sprintf('/api/v1/%s/schema', $engineName);
+    }
+
+}
