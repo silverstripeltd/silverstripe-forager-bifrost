17f2b110c8160f6ff88161675cc963228951b568
Chris Penny <chris.penny@gmail.com>
2024-06-17 10:14:43 +1200

    Update class name to PostDocumentsList

--- src/Service/Requests/PostDocumentsList.php
+++ src/Service/Requests/PostDocumentsList.php
@@ -4,7 +4,7 @@ namespace SilverStripe\ForagerBifrost\Service\Requests;
 
 use Elastic\EnterpriseSearch\AppSearch\Request\ListDocuments as AppSearchListDocuments;
 
-class GetDocumentsList extends AppSearchListDocuments
+class PostDocumentsList extends AppSearchListDocuments
 {
 
     public function __construct(string $engineName)
