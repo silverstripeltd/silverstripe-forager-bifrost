e22da891df0396def587df491116b1451fc5e0c3
Chris Penny <chris.penny@gmail.com>
2024-09-12 10:30:08 +1200

    Update GetEngines to PostEngines to allow for body content

--- src/Service/Requests/PostEngines.php
+++ src/Service/Requests/PostEngines.php
@@ -4,13 +4,14 @@ namespace SilverStripe\ForagerBifrost\Service\Requests;
 
 use Elastic\EnterpriseSearch\AppSearch\Request\ListEngines as AppSearchListEngines;
 
-class GetEngines extends AppSearchListEngines
+class PostEngines extends AppSearchListEngines
 {
 
     public function __construct()
     {
         parent::__construct();
 
+        $this->method = 'POST';
         $this->path = '/api/v1/engines';
     }
 
